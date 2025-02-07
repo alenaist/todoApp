fc83f9c745de285c8d126b1ef22bb2e02076774e
alejandro naistat <alenaist10@gmail.com>
2025-02-07 00:54:36 -0300

    removed pointer events from canvas

--- src/components/animatedAvatar/AnimatedAvatar.js
+++ src/components/animatedAvatar/AnimatedAvatar.js
@@ -368,6 +368,7 @@ const DebugAnimatedAvatar = ({ message, isLoadingApiCall }) => {
                 style={{
                     width: '100%',
                     height: '100%',
+                    pointerEvents: 'none',
                 }}
             />
         </div>
